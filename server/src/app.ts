b0f4e5fc9ec04fe6f71da35d30d104e92a2fa9a5
Alexandra <sashaf708@gmail.com>
2025-07-25 18:09:15 +0400

    feat: set up Express app with CORS and JSON middleware

---
+++ server/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import taskRoutes from './routes/tasks';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/tasks', taskRoutes);
+
+export default app;
